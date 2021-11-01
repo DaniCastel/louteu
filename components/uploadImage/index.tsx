695a87729dbd1978f60abcbd35b28aff91afdcb7
Daniela Castellanos <ldcastellanosa@gmail.com>
2021-11-01 07:37:24 -0500

    upload  image create category

--- components/uploadImage/index.tsx
+++ components/uploadImage/index.tsx
@@ -3,15 +3,7 @@ import { useState } from 'react';
 import { Upload } from 'antd';
 import ImgCrop from 'antd-img-crop';
 
-export default function UploadImage() {
-    const [fileList, setFileList] = useState([
-        {
-            uid: '-1',
-            name: 'image.png',
-            status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-        },
-    ]);
+export default function UploadImage({ fileList, setFileList }) {
 
 
     const onChange = ({ fileList: newFileList }) => {
@@ -34,15 +26,17 @@ export default function UploadImage() {
     };
 
 
-    return (<ImgCrop rotate>
-        <Upload
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            listType="picture-card"
-            fileList={fileList}
-            onChange={onChange}
-            onPreview={onPreview}
-        >
-            {fileList.length < 5 && '+ Upload'}
-        </Upload>
-    </ImgCrop>)
+    return (
+        <div><ImgCrop rotate>
+            <Upload
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                listType="picture-card"
+                fileList={fileList}
+                onChange={onChange}
+                onPreview={onPreview}
+            >
+                {fileList.length < 1 && '+ Upload'}
+            </Upload>
+        </ImgCrop>
+        </div>)
 }
--- pages/admin/category/create.tsx
+++ pages/admin/category/create.tsx
@@ -2,97 +2,100 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { API } from 'config';
 import { Form, Input, Checkbox, Button, Typography } from "antd";
-import UploadImage from 'components/uploadImage';
+import { openNotification } from "utils/toast";
 
 import withAdmin from "pages/withAdmin";
 
 const { Title } = Typography;
 
 
-type IUser = {
+interface ICategory {
     name: string;
     content: string;
     image: any;
 };
 
-const Create = ({ user, token }) => {
-    const [buttonText, setButtonText] = useState('')
 
-    const onFinish = async (values: IUser) => {
-        setButtonText('Creating');
-        console.log(values)
-        // console.log(...formData);
-        // formData.set(name, values.name);
-        // formData.set(content, values.name);
-        // formData.set(name, values.name);
 
-        // try {
-        //     const response = await axios.post(`${API}/category`, formData, {
-        //         headers: {
-        //             Authorization: `Bearer ${token}`
-        //         }
-        //     });
-        //     console.log('CATEGORY CREATE RESPONSE', response);
-        //     setState({
-        //         ...state,
-        //         buttonText: 'Created',
-        //         imageUploadText: 'Upload image',
-        //     });
-        // } catch (error) {
-        //     console.log('CATEGORY CREATE ERROR', error);
-        //     setState({ ...state, buttonText: 'Create', error: error.response.data.error });
-        // }
-    }
+const Create = ({ token }) => {
+    const [state, setState] = useState({
+        name: '',
+        content: '',
+        formData: process.browser && new FormData(),
+        buttonText: 'Create',
+        imageUploadText: 'Upload image'
+    });
 
-    const onFinishFailed = (errorInfo: any) => {
-        console.log("Failed:", errorInfo);
+    const { name, content, formData, buttonText, imageUploadText } = state;
+
+    const handleChange = name => e => {
+        const value = name === 'image' ? e.target.files[0] : e.target.value;
+        const imageName = name === 'image' ? event.target.files[0].name : 'Upload image';
+        formData.set(name, value);
+        setState({ ...state, [name]: value, imageUploadText: imageName });
     };
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setState({ ...state, buttonText: 'Creating' });
+        // console.log(...formData);
+        try {
+            const response = await axios.post(`${API}/category`, formData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            console.log('CATEGORY CREATE RESPONSE', response);
+            setState({
+                ...state,
+                name: '',
+                content: '',
+                formData: '',
+                buttonText: 'Created',
+                imageUploadText: 'Upload image',
+            });
+            openNotification("success", `${response.data.name} is created`);
+        } catch (error) {
+            console.log('CATEGORY CREATE ERROR', error);
+            setState({ ...state, name: '', buttonText: 'Create' });
+            // openNotification("error", error.response.data.error);
+        }
+    };
+
+    const createCategoryForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input onChange={handleChange('name')} value={name} type="text" className="form-control" required />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Content</label>
+                <textarea onChange={handleChange('content')} value={content} className="form-control" required />
+            </div>
+            <div className="form-group">
+                <label className="btn btn-outline-secondary">
+                    {imageUploadText}
+                    <input
+                        onChange={handleChange('image')}
+                        type="file"
+                        accept="image/*"
+                        className="form-control"
+                        hidden
+                    />
+                </label>
+            </div>
+            <div>
+                <button className="btn btn-outline-warning">{buttonText}</button>
+            </div>
+        </form>
+    );
+
 
     return (
         <div>
-            <Title>Create</Title>
-            <Form
-                name="basic"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 8,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    name="name"
-                    label="Name"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input your name!",
-                            whitespace: true,
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item
-                    name="content"
-                    label="Content"
-                    rules={[
-                        {
-                            required: true,
-                            message: "Please input the category content",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-                <UploadImage></UploadImage>
-            </Form>
+            <Title>Create category</Title>
+            <br />
+            {createCategoryForm()}
         </div>
     );
 };
