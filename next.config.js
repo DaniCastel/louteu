f939a0e50bc2d8bb451bdd26f43ad8da8e2bda87
Daniela Castellanos <ldcastellanosa@gmail.com>
2022-04-03 00:14:29 -0500

    use environment variables

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  reactStrictMode: true,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    APP_NAME: process.env.APP_NAME,
+    API: process.env.APP_NAME,
+    IS_PROD: process.env.IS_PROD,
+    DOMAIN: process.env.DOMAIN,
+    FB_APP_ID: process.env.FB_APP_ID,
+  },
+  i18n: {
+    /**
+     * Provide the locales you want to support in your application
+     */
+    locales: ["en", "es"],
+    /**
+     * This is the default locale you want to be used when visiting
+     * a non-locale prefixed path.
+     */
+    defaultLocale: "en",
+  },
+};
